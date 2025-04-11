da00550401bee7da33f864098c2b7c5e4b56668f
POTATOPROGAMMER <codercode1010@gmail.com>
2025-04-11 17:16:18 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,10 @@
+print("sets and its operations")
+set1={1,2,3,4,5,6,7}
+print(set1)
+set2={3,4,5,6,7,8,9}
+print(set2)
+print("union:",set1.union(set2))
+print("intersetion:",set1.intersection(set2))
+print("difference:",set1.difference(set2))
+print("symmetric difference:",set1.symmetric_difference(set2))
+
